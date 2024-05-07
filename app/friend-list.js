64141aa2152d8d8ace3b91f7e3e7ee4fb7841d6e
luizgadel <lpgds.eng16@uea.edu.br>
2024-05-07 02:34:34 -0400

    feat: adds tailwind support, styles various components

--- app/friend-list.js
+++ app/friend-list.js
@@ -24,18 +24,24 @@ export default function FriendList() {
     }
 
     return (
-        <>
-            <br />
-            <button onClick={handleClick}>Click me!</button>
+        <div className="border p-2 mx-96 mt-8 flex flex-col items-center">
+            <p>
+                Click the button below so I can show you some of my friends' names: 
+            </p>
 
-
-
-            <ul>
+            <button 
+                disabled={theEnd} 
+                className="my-4 w-24 h-8 bg-indigo-500 text-white rounded-full border disabled:bg-white disabled:text-black disabled:opacity-50 disabled:border-black" 
+                onClick={handleClick}
+            >
+                Click me!
+            </button>
+            <div className="flex flex-row flex-wrap">
                 {revealedList.map((name) => (
-                    <li key={name}>{name}</li>
+                    <span className="mx-2" key={name}>{name}</span>
                 ))}
-                { theEnd ? <li>...</li> : ''}
-            </ul>
-        </>
+                { theEnd ? <span>...</span> : ''}
+            </div>
+        </div>
     )
 }
--- app/page.js
+++ app/page.js
@@ -1,18 +1,26 @@
 import FriendList from "./friend-list";
+import Image from "next/image";
 
 function Header({title}) {
-    return <h1>{title ? title : 'Default title'}</h1>;
+    return <h1 className="text-3xl font-bold underline mx-auto"> {title ? title : 'Default title'}</h1>;
 }
 
 export default function HomePage() {
 
     return (
-        <div>
-            <Header title="Hello! My name is Luiz and I'm coding with Next.js " />
-            
-            Click the button below so I can show you some of my friends' names: 
+        <div className="flex flex-col container mx-auto">
+            <Header title="Hello, my name is Luiz. Nice to meet you!" />
             
             <FriendList />
+
+            <div className="mx-96 pt-4 px-4 mt-8 border">
+                This is my first app built with:
+                <div className="flex flex-row  items-center">
+                    <Image src="/nextjs.png"  width={100} height={50} alt="Next.js logo" className="my-auto mx-2"/>
+                    <span className="ms-4">AND</span>
+                    <Image src="/tailwindcss.png"  width={200} height={100} alt="Next.js logo" className="mx-2"/>
+                </div>
+            </div>
         </div>
     )
 }
